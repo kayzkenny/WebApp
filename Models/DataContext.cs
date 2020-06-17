04ddd691b466fa2ac171dea99243b86800619a09
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-06-17 16:59:06 +0100

    Create data model

---
+++ Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.Models
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> opts) : base(opts) { }
+        // This context class defines properties that will be used to query 
+        // the database for Product, Category, and Supplier data
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+## A simple web app built with asp.net core and entity framework core
